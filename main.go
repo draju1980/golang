4417cb5a5131df1a00f94a2113f9439d3a08550f
Divakar Raju Raju <draju@outlook.com>
2023-03-25 08:53:12 +0000

    added

--- main.go
+++ main.go
@@ -1,18 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+type logWriter struct{}
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff000",
-		"green": "#ff12G",
-		"blue":  "#ff12B",
+	resp, err := http.Get("http://google.com")
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
 	}
+	// bs := make([]byte, 99999)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
 
-	printMap(colors)
+	// io.Copy(os.Stdout, resp.Body)
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
 }
-func printMap(c map[string]string) {
-	for key, value := range c {
-		fmt.Println("value for", key, "is", value)
-	}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
 }
